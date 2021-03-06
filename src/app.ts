537eade84bf4a9c566e01aa535c1f2129005ab8d
sonchaegeon <thscorjs1002@naver.com>
2021-03-06 10:40:42 +0900

    UPDATE; set view engine, uuid, room router

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,21 @@
 import express from 'express';
 import http from 'http';
 import { Server } from 'socket.io';
+import { v4 } from 'uuid';
+
 const app = express();
 const io = new Server(new http.Server(app));
+
+app.set('view engine', 'ejs');
+app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+  res.redirect(`/${v4()}`);
+});
+
+app.get('/:room', (req, res) => {
+  res.render('room', { roomId: req.params.room });
+});
 
 app.listen(3000, () => {
   console.log('server running on port 3000');
