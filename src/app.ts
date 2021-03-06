9c5b91ec597f5174938ca25a593c4ccdef48fe79
sonchaegeon <thscorjs1002@naver.com>
2021-03-06 13:29:40 +0900

    FIX; socket setting

--- src/app.ts
+++ src/app.ts
@@ -2,12 +2,14 @@ import express from 'express';
 import http from 'http';
 import { Server, Socket } from 'socket.io';
 import { v4 } from 'uuid';
+import path from 'path';
 
 const app = express();
-const io = new Server(new http.Server(app));
+const server = http.createServer(app);
+const io = new Server(server);
 
 app.set('view engine', 'ejs');
-app.use(express.static('public'));
+app.use(express.static(__dirname + '/views/public'));
 
 app.get('/', (req, res) => {
   res.redirect(`/${v4()}`);
@@ -23,6 +25,6 @@ io.on('connection', (socket: Socket) => {
   });
 });
 
-app.listen(3000, () => {
+server.listen(3000, () => {
   console.log('server running on port 3000');
 });
