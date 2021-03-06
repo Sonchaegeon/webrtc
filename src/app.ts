50c5487b007e52e41719e50ab8b062200f176023
sonchaegeon <thscorjs1002@naver.com>
2021-03-06 10:35:03 +0900

    UPDATE; add socket.io setting

--- src/app.ts
+++ src/app.ts
@@ -1,2 +1,9 @@
 import express from 'express';
+import http from 'http';
+import { Server } from 'socket.io';
 const app = express();
+const io = new Server(new http.Server(app));
+
+app.listen(3000, () => {
+  console.log('server running on port 3000');
+});
